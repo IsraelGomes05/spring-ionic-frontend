7a36ead21d1f64ce5a8be72dccaaec43ef54569b
IsraelGomes05 <israelgomes05@gmail.com>
2018-10-05 01:06:43 -0400

    tratamento padrão para outros erros

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
-import {StorageService} from "../services/storage.service"; // IMPORTANTE: IMPORT ATUALIZADO
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {Observable} from 'rxjs/Rx';
+import {StorageService} from "../services/storage.service";
+import {AlertController} from "ionic-angular"; // IMPORTANTE: IMPORT ATUALIZADO
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
 
-  constructor(public storage : StorageService) {
+  constructor(public storage: StorageService,
+              public alertControler: AlertController) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -26,19 +28,51 @@ export class ErrorInterceptor implements HttpInterceptor {
         console.log(errorObj);
 
         switch (errorObj.status) {
+          case 401:
+            this.hundle401();
+            break;
           case 403:
             this.hundle403();
             break;
+          default:
+            this.hundleDefaultError(errorObj);
         }
-        
+
         return Observable.throw(errorObj);
       }) as any;
   }
 
-  hundle403(){
+  hundle403() {
     this.storage.setLocalUser(null);
   }
 
+  private hundle401() {
+    let alert = this.alertControler.create({
+      title: 'Erro 401: Falha de autenticação',
+      message: 'Email ou senha incorretos',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'OK'
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  private hundleDefaultError(errorObj) {
+    let alert = this.alertControler.create({
+      title: 'Erro ' +errorObj.status +': ' + errorObj.error,
+      message: errorObj.message,
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'OK'
+        }
+      ]
+    });
+    alert.present();
+  }
 }
 
 export const ErrorInterceptorProvider = {
