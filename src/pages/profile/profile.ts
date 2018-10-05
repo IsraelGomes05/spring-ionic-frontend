2946221246d7a1e419555c462f92e86f1ac2d82e
IsraelGomes05 <israelgomes05@gmail.com>
2018-10-05 00:53:34 -0400

    Tratando erro 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {StorageService} from "../../services/storage.service";
 import {ClienteDto} from "../../models/cliente.dto";
 import {ClienteService} from "../../services/domain/cliente.service";
@@ -13,28 +13,34 @@ import {API_CONFIG} from "../../config/api.config";
 })
 export class ProfilePage {
 
-  cliente : ClienteDto;
+  cliente: ClienteDto;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService : ClienteService) {
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
-    if (localUser && localUser.email){
+    if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
-        .subscribe(response =>{
-          this.cliente = response;
-          this.getImageIfExists();
-        },
-          error1 => {});
+        .subscribe(response => {
+            this.cliente = response;
+            this.getImageIfExists();
+          },
+          error1 => {
+            if (error1.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    } else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
-  getImageIfExists(){
+  getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
       .subscribe(response => {
           this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
