903d19a7520fe5568faed03cd4664aea253f6f6e
IsraelGomes05 <israelgomes05@gmail.com>
2018-10-09 23:14:52 -0400

    Carregando dados do produto

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {ProdutoDto} from "../../models/produtoDto";
+import {ProdutoService} from "../../services/domain/produto.service";
+import {API_CONFIG} from "../../config/api.config";
 
 @IonicPage()
 @Component({
@@ -9,16 +11,32 @@ import {ProdutoDto} from "../../models/produtoDto";
 })
 export class ProdutoDetailPage {
 
-  item : ProdutoDto;
+  item: ProdutoDto;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    this.item = {
-      id : '1',
-      nome : 'Mouse',
-      preco : 80.59
-    }
+    let produtoId = this.navParams.get('produto_id');
+    this.produtoService.findById(produtoId)
+      .subscribe(response => {
+          this.item = response;
+          this.getImageUrlIfExists();
+        },
+        error1 => {
+        });
+  }
+
+  getImageUrlIfExists() {
+    this.produtoService.getImageFromBucket(this.item.id)
+      .subscribe(response => {
+          this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
+        },
+        error1 => {
+        }
+      );
   }
 }
